4dc4f64d05e1050cd8196da50ff936b7b634b6a8
Robert <Robert.Elias@live.com>
2019-12-11 10:32:42 -0800

    axios request and img response

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import React from "react";
 import "./App.css";
 
+import { Container, Row } from "reactstrap";
+import React, {useEffect, useState} from "react";
+
+import axios from "axios";
+
 function App() {
+  const [photoOfTheDay, setPhotoOfTheDay] = useState('');
+    useEffect(()=> {
+      axios.get('https://api.nasa.gov/planetary/apod?api_key=YvRdRgaQrdbXP78gKmVFQ5osJmsSyrjwQ8gflbH6')
+      .then(response => {
+        console.log(response.data)
+        setPhotoOfTheDay(response.data)
+        //console.log(response)
+        //console.log(response.data.url)
+      })
+      .catch(error =>{
+        console.log("There is an error in the Axios call: ", error)
+      })
+    },[]);
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+    <div className="photoHolder" >
+    <img src={photoOfTheDay.url} alt={photoOfTheDay.title}></img>
+    </div>
+      
     </div>
   );
 }
